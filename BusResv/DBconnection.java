e0641aca0c909754dea1e60614cf165a53eae4a1
Nisanthnis <129824637+Nisanthnis@users.noreply.github.com>
2023-11-29 18:45:24 +0530

    Update DBconnection.java

--- BusResv/DBconnection.java
+++ BusResv/DBconnection.java
@@ -6,7 +6,7 @@ import java.sql.*;
 public class DBconnection {
 	 public static final String url="jdbc:mysql://localhost:3306/busresv";
 	 public static final String username="root";
-	 public static final String password="Nisanaj@1303";
+	 public static final String password="nisa1234";
 	 
 	 public static Connection getConnection() throws SQLException{
 		 return DriverManager.getConnection(url,username,password);
